b91699dcfe32dc0365c5d7fcde73cbabd7f64ff5
Auston Hefling <auston.hefling@outlook.com>
2018-09-25 09:34:48 -0400

    Start

---
+++ Lab8B_ASP/Controllers/HomeController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Lab8B_ASP.Models;
+
+namespace Lab8B_ASP.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public string Index()
+        {
+            return "Navigate to a URL to show an example";
+        }
+
+        public ViewResult AutoProperty()
+        {
+            Product myProduct = new Product();
+            myProduct.Name = "Kayak";
+            string productName = myProduct.Name;
+            return View("Result", (Object)String.Format("Product name: {0}", productName));
+        }
+
+        public ViewResult CreateProduct()
+        {
+            Product myProduct = new Product
+            {
+                ProductID = 100,
+                Name = "Kayak",
+                Description = "A boat for one peron",
+                Price = 275M,
+                Category = "Watersports"
+            };
+            return View("Result", (object)String.Format("Category: {0}", myProduct.Category));
+        }
+
+        public ViewResult CreateCollection()
+        {
+            string[] stringArray = { "Apple", "Orange", "Plum" };
+            List<int> intList = new List<int> { 10, 20, 30, 40 };
+            Dictionary<string, int> myDict = new Dictionary<string, int>
+            {
+                {"Apple", 10}, {"Orange", 20}, {"Plum", 30}
+            };
+            return View("Result", (object)stringArray[1]);
+        }
+    }
+}
---
+++ Lab8B_ASP/Models/ShoppingCart.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Lab8B_ASP.Models
+{
+    public class ShoppingCart
+    {
+    }
+}
