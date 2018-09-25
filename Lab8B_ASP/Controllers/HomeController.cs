39d5b55f608f1e23bf1dc7639db430fb04bb8b2b
Auston Hefling <auston.hefling@outlook.com>
2018-09-25 10:04:53 -0400

    Sendit

--- Lab8B_ASP/Controllers/HomeController.cs
+++ Lab8B_ASP/Controllers/HomeController.cs
@@ -20,7 +20,7 @@ namespace Lab8B_ASP.Controllers
             Product myProduct = new Product();
             myProduct.Name = "Kayak";
             string productName = myProduct.Name;
-            return View("Result", (Object)String.Format("Product name: {0}", productName));
+            return View("Result", (object)String.Format("Product name: {0}", productName));
         }
 
         public ViewResult CreateProduct()
@@ -45,6 +45,66 @@ namespace Lab8B_ASP.Controllers
                 {"Apple", 10}, {"Orange", 20}, {"Plum", 30}
             };
             return View("Result", (object)stringArray[1]);
+        }
+
+        public ViewResult UseExtensionEnumerable()
+        {
+            IEnumerable<Product> products = new ShoppingCart
+            {
+                Products = new List<Product>
+                {
+                    new Product {Name = "Kayak", Price = 275M},
+                    new Product {Name = "LifeJacket", Price = 48.95M},
+                    new Product {Name = "Soccer Ball", Price = 19.50M},
+                    new Product {Name = "Corner Flag", Price = 34.95M}
+                }
+            };
+            Product[] productArray =
+            {
+                new Product {Name = "Kayak", Price = 275M},
+                new Product {Name = "LifeJacket", Price = 48.95M},
+                new Product {Name = "Soccer Ball", Price = 19.50M},
+                new Product {Name = "Corner Flag", Price = 34.95M}
+            };
+            decimal cartTotal = products.TotalPrices();
+            decimal arrayTotal = products.TotalPrices();
+            return View("Result", (object)String.Format("Cart Total: {0}, Array Total: {1}", cartTotal, arrayTotal));
+        }
+
+        public ViewResult UseExtensions()
+        {
+            ShoppingCart cart = new ShoppingCart
+            {
+                Products = new List<Product>
+                {
+                    new Product {Name = "Kayak", Price = 275M},
+                    new Product {Name = "LifeJacket", Price = 48.95M},
+                    new Product {Name = "Soccer Ball", Price = 19.50M},
+                    new Product {Name = "Corner Flag", Price = 34.95M}
+                }
+            };
+            decimal cartTotal = cart.TotalPrices();
+            return View("Result", (object)String.Format("Total: {0:c}", cartTotal));
+        }
+
+        public ViewResult UseFilterExtensionMethod()
+        {
+            IEnumerable<Product> products = new ShoppingCart
+            {
+                Products = new List<Product>
+                {
+                    new Product {Name = "Kayak", Price = 275M},
+                    new Product {Name = "LifeJacket", Price = 48.95M},
+                    new Product {Name = "Soccer Ball", Price = 19.50M},
+                    new Product {Name = "Corner Flag", Price = 34.95M}
+                }
+            };
+            decimal total = 0;
+            foreach (Product prod in products.FilterByCategory("Soccer"))
+            {
+                total += prod.Price;
+            }
+            return View("Result", (object)String.Format("Total: {0}", total));
         }
     }
 }
--- Lab8B_ASP/Models/ShoppingCart.cs
+++ Lab8B_ASP/Models/ShoppingCart.cs
@@ -1,11 +1,21 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections;
 using System.Linq;
 using System.Web;
 
 namespace Lab8B_ASP.Models
 {
-    public class ShoppingCart
+    public class ShoppingCart : IEnumerable<Product>
     {
+        public List<Product> Products { get; set; }
+        public IEnumerator<Product> GetEnumerator()
+        {
+            return Products.GetEnumerator();
+        }
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
     }
 }
